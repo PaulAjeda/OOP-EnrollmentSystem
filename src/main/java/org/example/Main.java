61eee9b6b7d825ae7c537a1105da9061ca5ad430
Paul Geneo P. Ajeda <ajedapaulgeneo23@gmail.com>
2026-03-28 01:27:55 +0800

    Service Layer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,113 @@
 package org.example;
 
+import org.example.model.Course;
+import org.example.model.Student;
+import org.example.service.StudentRegistration;
+import org.example.service.CourseRegistration;
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String name = "";
+    static String program = "";
+
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setStudentID("000123");
-        student.setStudentName("John Doe");
-        student.setProgram("Information Technology");
+        Scanner hh = new Scanner(System.in);
 
-        student.display();
+        StudentRegistration studentRegistration = new StudentRegistration();
 
-        Student student2 = new Student();
-        student2.setStudentID("000124");
-        student2.setStudentName("Jane Doe");
-        student2.setProgram("Information Technology");
+        int choice;
+        int studID;
+        String studName = "";
+        String program = "";
 
-        student2.display();
+        do {
+            System.out.println("[0] Exit");
+            System.out.println("[1] Save Student");
+            System.out.println("[2] Display Student");
+            System.out.println("[3] Update Student");
+            System.out.println("[4] Remove Student");
 
-        Course course = new Course();
-        course.setCourseID("0001");
-        course.setCourseName("Integrative Programming");
-        course.setProgram("Information Technology");
+            System.out.println("Enter choice: ");
+            choice = hh.nextInt();
+            hh.nextLine();
 
-        course.display();
+            switch (choice) {
+                case 1:
+                    //Create
+                    System.out.print("Enter student ID: ");
+                    studID = hh.nextInt();
+                    hh.nextLine();
 
-        Course course2 = new Course();
-        course2.setCourseID("0001");
-        course2.setCourseName("Integrative Programming");
-        course2.setProgram("Information Technology");
+                    System.out.print("Enter student name: ");
+                    studName = hh.nextLine();
 
-        course2.display();
+                    System.out.print("Enter program: ");
+                    program = hh.nextLine();
 
+                    studentRegistration.addStudent(new Student(studID, studName, program));
+                    break;
+
+                case 2:
+                    //Read
+
+                    studentRegistration.displayAll();
+                    break;
+
+                case 3:
+                    //Update
+                    System.out.print("Enter student ID to be updated: ");
+                    studID = hh.nextInt();
+                    hh.nextLine();
+
+                    studentRegistration.updateStudent(new Student(studID));
+                    break;
+
+                case 4:
+                    //Delete
+                    System.out.print("Enter student ID to be removed: ");
+                    studID = hh.nextInt();
+                    hh.nextLine();
+
+                    studentRegistration.delete(new Student(studID));
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        } while (choice != 0);
     }
 }
+//    public static void main(String[] args) {
+//        Student student = new Student();
+//        student.setStudentID("000123");
+//        student.setStudentName("John Doe");
+//        student.setProgram("Information Technology");
+//
+//        student.display();
+//
+//        Student student2 = new Student();
+//        student2.setStudentID("000124");
+//        student2.setStudentName("Jane Doe");
+//        student2.setProgram("Information Technology");
+//
+//        student2.display();
+//
+//        Course course = new Course();
+//        course.setCourseID("0001");
+//        course.setCourseName("Integrative Programming");
+//        course.setProgram("Information Technology");
+//
+//        course.display();
+//
+//        Course course2 = new Course();
+//        course2.setCourseID("0001");
+//        course2.setCourseName("Integrative Programming");
+//        course2.setProgram("Information Technology");
+//
+//        course2.display();
+//
+//    }
+//}
--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,33 +1,41 @@
-package org.example;
+package org.example.model;
 
 public class Student {
-    private String studentID;
+    private int StudentID;
     private String studentName;
     private String program;
 
     public Student(){
+        this(0);
     }
 
-    public Student(String studentID, String studentName, String program){
-        this.studentID = studentID;
-        this.studentName = studentName;
+    public Student(int StudentID){
+        this(StudentID, "Unknown");
+    }
+
+    public Student(int StudentID, String studentName){
+        this(StudentID, studentName, "Information Technology");
+        }
+
+        public Student(int ID, String name, String program) {
+
+        this.StudentID = ID;
+        this.studentName = name;
         this.program = program;
     }
 
-    public String getStudentID(){
-        return studentID;
-    }
+    public int getStudentID(){ return StudentID; }
 
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
+/*    public void setStudentID(String studentID) {
+        this.StudentID = studentID;
     }
-
+*/
     public String getStudentName(){
         return studentName;
     }
 
-    public void setStudentName(String studentName){
-        this.studentName = studentName;
+    public void setName(String name){
+        this.studentName = name;
     }
 
     public String getProgram(){
@@ -43,5 +51,14 @@ public class Student {
         System.out.println("Student Name:" + getStudentName());
         System.out.println("Program:" + getProgram());
     }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "StudentID=" + StudentID +
+                ", studentName='" + studentName + '\'' +
+                ", program='" + program + '\'' +
+                '}';
+    }
 }
+
 
